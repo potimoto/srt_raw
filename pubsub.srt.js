57240cc618b7976ede875cd63184e70af1d6aa7e
瀧本恒平 <kouheitakimoto@hanmayuujirounoMacBook-Pro.local>
2023-02-08 13:13:36 +0900

    first commit

---
+++ pubsub.srt.js
@@ -0,0 +1,24 @@
+1
+00:10:53,000 --> 00:11:03,500
+const turtle = [0.05,0.3];
+controller(turtle);
+
+2
+00:11:03,501 --> 00:12:21,999
+const turtle = [0.00,0.00];
+controller(turtle);
+
+3
+00:12:22,000 --> 00:12:27,000
+const turtle = [0.1,0.00];
+controller(turtle);
+
+4
+00:12:27,001 --> 00:12:29,001
+const turtle = [0.00,0.785];
+controller(turtle);
+
+5
+00:12:29,002 --> 00:13:53,000
+const turtle = [0.00,0.00];
+controller(turtle);
